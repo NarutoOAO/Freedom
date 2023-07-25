3d356c186f697c7317454bff000e4af2acb4a9bc
NarutoOAO <33660983+NarutoOAO@users.noreply.github.com>
2023-07-26 00:58:14 +1000

    Delete frontend directory

--- frontend/src/pages/OnlineClass/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function OnlineClass() {
-  return (
-    <div className='createQuiz'>
-      <h1>OnlineClass</h1>
-    </div>
-  )
-}
--- frontend/src/pages/SetAssignment/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function SetAssignment() {
-  return (
-    <div className='createQuiz'>
-      <h1>SetAssignment</h1>
-    </div>
-  )
-}
